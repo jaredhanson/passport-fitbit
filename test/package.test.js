fe9e168a28c943b1ff5042c48521e8225bcb7c6a
Jared Hanson <jaredhanson@gmail.com>
2016-02-04 09:07:26 -0800

    Update support files.

---
+++ test/package.test.js
@@ -0,0 +1,18 @@
+/* global describe, it */
+
+var strategy = require('..');
+var expect = require('chai').expect;
+
+
+describe('passport-fitbit', function() {
+  
+  it('should export Strategy constructor', function() {
+    expect(strategy.Strategy).to.be.a('function');
+  });
+    
+  it('should export Strategy constructor as module', function() {
+    expect(strategy).to.be.a('function');
+    expect(strategy).to.equal(strategy.Strategy);
+  });
+  
+});
