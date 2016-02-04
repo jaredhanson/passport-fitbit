0b07382bf7fbf44c8550152a8824a177848bb475
Jared Hanson <jaredhanson@gmail.com>
2016-02-04 09:17:53 -0800

    Internal error test case.

--- test/strategy.profile.test.js
+++ test/strategy.profile.test.js
@@ -46,4 +46,37 @@ describe('Strategy#userProfile', function() {
     });
   }); // fetched from default endpoint
   
+  describe('internal error', function() {
+    var strategy = new FitBitStrategy({
+      consumerKey: 'ABC123',
+      consumerSecret: 'secret'
+    }, function verify(){});
+    
+    strategy._oauth.get = function(url, token, tokenSecret, callback) {
+      return callback(new Error('something went wrong'));
+    }
+    
+    
+    var err, profile;
+    before(function(done) {
+      strategy.userProfile('token', 'token-secret', { user_id: '123' }, function(e, p) {
+        err = e;
+        profile = p;
+        done();
+      });
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.constructor.name).to.equal('InternalOAuthError');
+      expect(err.message).to.equal('Failed to fetch user profile');
+      expect(err.oauthError).to.be.an.instanceOf(Error);
+      expect(err.oauthError.message).to.equal('something went wrong');
+    });
+  
+    it('should not supply profile', function() {
+      expect(profile).to.be.undefined;
+    });
+  }); // internal error
+  
 });
